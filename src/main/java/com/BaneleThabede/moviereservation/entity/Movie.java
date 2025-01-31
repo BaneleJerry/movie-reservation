7f9b5bc35ea907aaf9dda825e3a400ea62cecad0
Banele Thabede <banelethabede@outlook.com>
2025-01-31 22:35:23 +0200

    entity fixes

---
+++ src/main/java/com/BaneleThabede/moviereservation/entity/Movie.java
@@ -0,0 +1,40 @@
+package com.BaneleThabede.moviereservation.entity;
+
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @Column(nullable = false)
+    private String posterUrl;
+
+    @OneToMany(mappedBy = "movie", cascade = CascadeType.ALL)
+    private List<Showtime> showtimes;
+
+    // Getters and Setters
+}
+
--- src/main/java/com/BaneleThabede/moviereservation/entity/Showtime.java
+++ src/main/java/com/BaneleThabede/moviereservation/entity/Showtime.java
@@ -1,6 +1,43 @@
 package com.BaneleThabede.moviereservation.entity;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="showtime")
 public class Showtime {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name="movie_id", nullable=false)
+    private Movie Movie;
+
+    @Column(nullable=false)
+    private LocalDateTime showtime;
+
+    @Column(nullable=false)
+    private int totalSeats;
+
+    @OneToMany(mappedBy="showtime", cascade= CascadeType.ALL)
+    private Seat seat;
 
 }
 
